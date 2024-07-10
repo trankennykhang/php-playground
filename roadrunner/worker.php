72ca4034c5c367926f8c0a92ae6140a640d1f17a
Kenny Tran <trankennykhang@hotmail.com>
2024-07-10 23:07:17 +0930

    roadrunner test code

--- roadrunner/worker.php
+++ roadrunner/worker.php
@@ -1,22 +1,35 @@
 <?php
 
-use Spiral\RoadRunner;
-use Nyholm\Psr7;
+require __DIR__ . '/vendor/autoload.php';
 
-include "vendor/autoload.php";
+use Nyholm\Psr7\Response;
+use Nyholm\Psr7\Factory\Psr17Factory;
 
-$worker = RoadRunner\Worker::create();
-$psrFactory = new Psr7\Factory\Psr17Factory();
+use Spiral\RoadRunner\Worker;
+use Spiral\RoadRunner\Http\PSR7Worker;
+$worker = Worker::create();
 
-$worker = new RoadRunner\Http\PSR7Worker($worker, $psrFactory, $psrFactory, $psrFactory);
+$factory = new Psr17Factory();
+$rad =rand(1,20);
+print "kenny {$rad}";
+$psr7 = new PSR7Worker($worker, $factory, $factory, $factory);
 
-while ($req = $worker->waitRequest()) {
+while (true) {
     try {
-        $rsp = new Psr7\Response();
-        $rsp->getBody()->write('Hello world!');
-
-        $worker->respond($rsp);
+        $request = $psr7->waitRequest();
+        echo "kkkk {$rad}\n";
+        if ($request === null) {
+            break;
+        }
     } catch (\Throwable $e) {
-        $worker->getWorker()->error((string)$e);
+        $psr7->respond(new Response(400));
+        continue;
+    }
+
+    try {
+        $psr7->respond(new Response(200, [], 'Hello RoadRunner!'));
+    } catch (\Throwable $e) {
+        $psr7->respond(new Response(500, [], 'Something Went Wrong!'));
+        $psr7->getWorker()->error((string)$e);
     }
 }
