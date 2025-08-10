121176f6fa218cd002a434c33ee2983d440cece3
kenny <trankennykhang@hotmail.com>
2025-08-10 22:09:08 +0930

    commit all

--- roadrunner/run.sh
+++ roadrunner/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/www/html
-composer update
+#composer update
 rr serve -c /var/www/html/.rr.yaml
--- roadrunner/test.php
+++ roadrunner/test.php
@@ -1,48 +1,60 @@
 <?php
 error_reporting(E_ALL);
 
-echo "<h2>TCP/IP Connection</h2>\n";
+/* Allow the script to hang around waiting for connections. */
+set_time_limit(0);
 
-/* Get the port for the WWW service. */
-$service_port = 6002;
+/* Turn on implicit output flushing so we see what we're getting
+ * as it comes in. */
+ob_implicit_flush();
 
-/* Get the IP address for the target host. */
 $address = '127.0.0.1';
-
-/* Create a TCP/IP socket. */
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-if ($socket === false) {
+$port = 6002;
+print 4444;
+if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
     echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
-} else {
-    echo "OK.\n";
 }
 
-echo "Attempting to connect to '$address' on port '$service_port'...";
-$result = socket_connect($socket, $address, $service_port);
-if ($result === false) {
-    echo "socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
-} else {
-    echo "OK.\n";
+if (socket_bind($sock, $address, $port) === false) {
+    echo "socket_bind() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
 }
 
-$in = "HEAD / HTTP/1.1\r\n";
-$in .= "Host: www.example.com\r\n";
-$in .= "Connection: Close\r\n\r\n";
-$out = '';
-
-echo "Sending HTTP HEAD request...";
-echo "OK.\n";
-
-echo "Reading response:\n\n";
-$buf = 'This is my buffer.';
-if (false !== ($bytes = socket_recv($socket, $buf, 2048, MSG_WAITALL))) {
-    echo "Read $bytes bytes from socket_recv(). Closing socket...";
-} else {
-    echo "socket_recv() failed; reason: " . socket_strerror(socket_last_error($socket)) . "\n";
+if (socket_listen($sock, 5) === false) {
+    echo "socket_listen() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
 }
-//socket_write($socket, $in, strlen($in));
-socket_close($socket);
 
-echo $buf . "\n";
-echo "OK.\n\n";
+do {
+    if (($msgsock = socket_accept($sock)) === false) {
+        echo "socket_accept() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
+        break;
+    }
+    /* Send instructions. */
+    $msg = "\nWelcome to the PHP Test Server. \n" .
+        "To quit, type 'quit'. To shut down the server type 'shutdown'.\n";
+    //socket_write($msgsock, $msg, strlen($msg));
+
+    do {
+        if (false === ($buf = socket_read($msgsock, 2048, PHP_NORMAL_READ))) {
+            echo "socket_read() failed: reason: " . socket_strerror(socket_last_error($msgsock)) . "\n";
+            break 2;
+        }
+        if (!$buf = trim($buf)) {
+            continue;
+        }
+        if ($buf == 'quit') {
+            break;
+        }
+        if ($buf == 'shutdown') {
+            socket_close($msgsock);
+            break 2;
+        }
+        print 3444;
+        $talkback = "PHP: You said '$buf'.\n";
+        socket_write($msgsock, $talkback, strlen($talkback));
+        echo "$buf\n";
+    } while (true);
+    socket_close($msgsock);
+} while (true);
+
+socket_close($sock);
 ?>
